6765921960a26c3df5cf2cf9c48ea03e0c09b6c5
ryuselmi <yuselmirodriguez24@gmail.com>
2024-07-09 16:00:00 -0700

    4:00pm

--- Program.cs
+++ Program.cs
@@ -1,10 +1,52 @@
 ﻿namespace SearchingAlgorithms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr1 = {1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int arr1Result = LinearSearch(arr1, 4);
+            int maxValue = arr1.Max();
+            int minValue = arr1.Min();
+            Console.WriteLine(arr1Result);
+            Console.WriteLine("The max value is: " + maxValue);
+            Console.WriteLine("The min value is: " + minValue);
+
+            Console.ReadKey();
+        }
+
+        public static int LinearSearch(int[] arrToSearch, int target)
+        {
+
+            for (int i = 0; i < arrToSearch.Length; i++)
+            {
+                if (arrToSearch[i] == target) return i;
+            }
+
+            return -1;
+        }
+
+        public static int BinarySearch(int[] arrToSearch, int target) 
+        {
+            int lower = 0;
+            int upper = arrToSearch.Length - 1;
+            
+            while (upper > lower)
+            {
+                int mid = (lower  + upper) / 2;
+                if (arrToSearch[mid] == target) return mid;
+                else if (target < arrToSearch[mid])
+                {
+                    upper = mid - 1;
+                }
+                else
+                {
+                    lower = mid + 1;
+                }
+            }
+
+            return -1;
         }
     }
 }
+
